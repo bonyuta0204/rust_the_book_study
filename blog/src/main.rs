c14345b4d23e7e6dff74736f092632e9d0570085
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-01-06 14:43:43 +0900

    feat: add blogs

---
+++ blog/src/main.rs
@@ -0,0 +1,17 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate salad for lunch");
+
+    assert_eq!("", post.content());
+
+    post.request_review();
+
+    assert_eq!("", post.content());
+
+    post.approve();
+
+    assert_eq!("I ate salad for lunch", post.content());
+}
